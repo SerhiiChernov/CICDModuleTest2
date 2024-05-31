478fb43b0abd007a0acc2624d15b0c64b95f26fd
Serhii-Chernov <chernovsrg@gmail.com>
2024-05-31 12:55:15 +0300

    added migrations

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from recipe import views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main, name='main'),
+    path('recipes/', views.recipe_list, name='recipe_list'),
 ]
